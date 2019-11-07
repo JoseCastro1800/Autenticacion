9413557a40d262730c703559e314ce3499644151
JoseCastro1800 <55769727+JoseCastro1800@users.noreply.github.com>
2019-11-06 21:28:03 -0800

    Add files via upload

---
+++ Autenticacion/Autenticacion/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Autenticacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Registro r = new Registro();
+            Usuario u = new Usuario();
+            u.Nombre = "Jose Castro";
+            u.Username = "Manolo";
+            u.Password = "963214";
+            r.RegistrarUsuario(u, "archivo.txt");
+
+            
+            Login L = new Login();
+            L.Bienvenido();
+            L.Ingresar_Datos();
+
+            Console.ReadKey();
+
+
+
+            
+        }
+
+       
+
+
+            
+
+
+        
+
+
+    }
+}
---
+++ Autenticacion/Autenticacion/Registro.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace Autenticacion
+{
+    public class Registro
+    {
+
+        public void RegistrarUsuario(Usuario u, string path)
+        {
+            string data = u.Nombre + "," + u.Username + "," + u.Password;
+            var datos = ObtenerLineas(path);
+            if (datos != null)
+            {
+                datos.Add(data);
+                File.WriteAllLines(path, datos);
+            }
+            else
+            {
+
+                File.WriteAllText(path, data);
+            }
+        }
+
+        public List<string> ObtenerLineas(string ruta)
+        {
+            string[] data = null;
+            //Esto es ara saber si existe el archivo
+            if (File.Exists(ruta))
+            {
+                data = File.ReadAllLines(ruta);
+            }
+            //Sirve para comprobar si hay un archivo si no nos mandaria un error
+            else
+            {
+                Console.WriteLine("No existe archivo");
+                return null;
+            }
+            
+
+            List<string> datos = new List<string>();
+
+            foreach (var item in data)
+            {
+                datos.Add(item);
+            }
+
+            return datos;
+        }
+
+
+
+    }
+}
